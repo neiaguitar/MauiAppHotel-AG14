170dbdab07204dab1b57d5c920a1e42efe15bf1f
Admin <Admin@ENEIAS-DESKTOP>
2024-11-16 16:22:50 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppHotel/MauiProgram.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace MauiAppHotel
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Kalam-Bold.ttf", "KalamBold");
+                    fonts.AddFont("Kalam-Light.ttf", "KalamLight");
+                    fonts.AddFont("Kalam-Regular.ttf", "Kalam");
+                    fonts.AddFont("Kanit-Regular.ttf", "Kanit");
+                    fonts.AddFont("SourGummy.ttf", "SourGummy");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
---
+++ MauiAppHotel/Views/AjudaPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace MauiAppHotel.Views;
+
+public partial class AjudaPage : ContentPage
+{
+	public AjudaPage()
+	{
+		InitializeComponent();
+	}
+}
